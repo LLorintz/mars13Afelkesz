5f02a6483e9823873c2e35207161e6afe122c47a
Lőrintz Levente <lorintz.levente@burg.lo.hu>
2024-10-25 11:42:02 +0200

    localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,44 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import './app.css'
 import PlayerName from './components/PlayerName'
 import Resource from './components/Resource'
 import { Resourceprops } from './components/Resource'
 function App() {
+  
+  const handleIncrement=(increment:number, resource:number, callback:(value:number)=>void)=>{
+    callback(resource+increment);
+  }
 
-  const [Megacredit, setMegacredit] = useState<number>(0)
-  const [MegacreditProduction, setMegacreditProduction] = useState<number>(0)
-  const handleMegacreditChange=(increment:number)=>{setMegacredit(Megacredit+increment)}
-  const handleMegacreditProductionChange=(increment:number)=>{setMegacreditProduction(MegacreditProduction+increment)}
-  const MegacreditObj:Resourceprops = 
+  const load=(resource:string)=>{
+    const saved = localStorage.getItem(resource);
+    return saved ? Number(saved) : 0;
+  }
+  const [Megacredit, setMegacredit] = useState<number>(load("megacredit"))
+  const [MegacreditProduction, setMegacreditProduction] = useState<number>(load("megacreditProduction"))
+   const MegacreditObj:Resourceprops = 
   {name:"Megacredit",
   amount:Megacredit,
   production:MegacreditProduction,
-  onAmountChange:handleMegacreditChange,
-  onProductionChange: handleMegacreditProductionChange}
+  onAmountChange:(increment:number)=>handleIncrement(increment,Megacredit,setMegacredit),
+  onProductionChange: (increment:number)=>handleIncrement(increment,MegacreditProduction,setMegacreditProduction)}
  //Acél
-  const [Steel, setSteel] = useState<number>(0)
-  const [SteelProduction, setSteelProduction] = useState<number>(0)
-  const handleSteelChange=(increment:number)=>{setSteel(Steel+increment)}
-  const handleSteelPRoductionChange=(increment:number)=>{setSteelProduction(SteelProduction+increment)}
+  const [Steel, setSteel] = useState<number>(load("steel"))
+  const [SteelProduction, setSteelProduction] = useState<number>(load("steelProduction"))
   const SteelObj:Resourceprops = 
   {name:"Steel",
    amount:Steel,
    production:SteelProduction,
-   onAmountChange:handleSteelChange,
-  onProductionChange:handleSteelPRoductionChange}
+   onAmountChange:(increment:number)=>handleIncrement(increment,Steel,setSteel),
+  onProductionChange:(increment:number)=>handleIncrement(increment,SteelProduction,setSteelProduction)}
  //Titán
- const [Titan, setTitan] = useState<number>(0)
- const [TitanProduction, setTitanProduction] = useState<number>(0)
- const handleTitanChange=(increment:number)=>{setTitan(Titan+increment)}
- const handleTitanProductionChange=(increment:number)=>{setTitanProduction(TitanProduction+increment)}
+ const [Titan, setTitan] = useState<number>(load('titan'))
+ const [TitanProduction, setTitanProduction] = useState<number>(load('titanProduction'))
  const TitanObj:Resourceprops = 
  {name:"Titan",
   amount:Titan,
   production:TitanProduction,
-  onAmountChange:handleTitanChange,
-  onProductionChange:handleTitanProductionChange
+  onAmountChange:(increment:number)=>handleIncrement(increment,Titan,setTitan),
+  onProductionChange:(increment:number)=>handleIncrement(increment,TitanProduction,setTitanProduction)
 }
 
 const handleSubmit=(e:FormEvent)=>{
@@ -45,6 +47,15 @@ const handleSubmit=(e:FormEvent)=>{
   setSteel(Steel+SteelProduction);
   setTitan(Titan+TitanProduction);
 }
+
+useEffect(()=>{
+  localStorage.setItem("megacredit", Megacredit.toString());
+  localStorage.setItem("megacreditProduction", MegacreditProduction.toString());
+  localStorage.setItem("steel", Steel.toString());
+  localStorage.setItem("steelProduction", SteelProduction.toString());
+  localStorage.setItem("titan", Titan.toString());
+  localStorage.setItem("titanProduction", TitanProduction.toString());
+},[Megacredit, MegacreditProduction,Steel,SteelProduction, Titan, TitanProduction])
 
   return (
     <form onSubmit={handleSubmit} className="container">
